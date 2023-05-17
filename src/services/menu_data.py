f3bb7b36f19f70e4dce5d760c8be0b812756eda4
evertonpsantos <evertonpsantos8@gmail.com>
2023-05-17 20:27:00 -0300

    feat: implements MenuData class

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,30 @@
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self.populate_menu(source_path)
+
+    def populate_menu(self, path):
+        menu = dict()
+        file_content = self.read_file_data(path)
+
+        for dish in file_content:
+            new_dish = Dish(dish["dish"], float(dish["price"]))
+
+            if new_dish not in menu:
+                menu[new_dish] = new_dish
+
+            menu[new_dish].add_ingredient_dependency(
+                Ingredient(dish["ingredient"]),
+                int(dish["recipe_amount"])
+            )
+
+        return set(menu)
+
+    def read_file_data(self, source_path):
+        with open(source_path) as file:
+            return list(csv.DictReader(file))
